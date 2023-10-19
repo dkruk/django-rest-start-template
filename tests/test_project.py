66bfd704fc0fa1a61a850ed567a0744342889946
Dzmitry <11198493+dkruk@users.noreply.github.com>
2023-10-19 10:55:11 +0300

    Move settings from 'setup.cfg' to 'pyproject.toml'

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,9 +3,9 @@ from django.conf import settings
 from src import __version__
 
 
-def test_version():
+def test_version() -> None:
     assert __version__ == '0.1.0'
 
 
-def test_settings():
+def test_settings() -> None:
     assert settings.IS_TEST
